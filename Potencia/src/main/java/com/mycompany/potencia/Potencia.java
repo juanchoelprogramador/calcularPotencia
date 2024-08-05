4f06222fa07761cb546c434053513e8a9392cfab
juancho el programador <juandavidneusa04@gmail.com>
2024-08-05 10:09:29 -0500

    Se calcula la potencia

--- Potencia/src/main/java/com/mycompany/potencia/Potencia.java
+++ Potencia/src/main/java/com/mycompany/potencia/Potencia.java
@@ -4,13 +4,23 @@
 
 package com.mycompany.potencia;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
  */
 public class Potencia {
+    
+    public static double calcularPotencia(double n1, double n2){
+        return Math.pow(n1, n2);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner lector = new Scanner(System.in);
+        double numero1 = lector.nextDouble();
+        double numero2 = lector.nextDouble();
+        double respuesta = calcularPotencia(numero1, numero2);
+        System.out.println("La potencia es " + respuesta);
     }
 }
