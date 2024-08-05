9a1fcbec7491f619166320a883dbf59f94fb0ca8
juancho el programador <juandavidneusa04@gmail.com>
2024-08-05 10:03:59 -0500

    Se crea el proyecto de Netbeans

---
+++ Potencia/src/main/java/com/mycompany/potencia/Potencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.potencia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Potencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
